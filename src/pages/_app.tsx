c0e84426db05a5dc72779254d634de05c669dee0
Andre Melo <andre.fabian.melo@gmail.com>
2020-09-18 01:07:34 -0300

    NextJS template TS, Eslint, Styled Components

---
+++ src/pages/_app.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+
+import React from 'react';
+import { AppProps } from 'next/app';
+
+import GlobalStyle from '../styles/global';
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </>
+  );
+};
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Head from 'next/head';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>HELLOW FUCKERS</h1>
+        <h1>HELLO WORLD</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
