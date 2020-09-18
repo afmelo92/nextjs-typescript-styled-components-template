51c57685628098cbae22e4e7bf193651a668a283
Andre Melo <andre.fabian.melo@gmail.com>
2020-09-18 01:36:32 -0300

    Update theme provider and head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,16 @@
-/* eslint-disable import/extensions */
-/* eslint-disable import/no-unresolved */
-
 import React from 'react';
 import { AppProps } from 'next/app';
 
+import { ThemeProvider } from 'styled-components';
 import GlobalStyle from '../styles/global';
+import theme from '../styles/theme';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Component {...pageProps} />
       <GlobalStyle />
-    </>
+    </ThemeProvider>
   );
 };
 
